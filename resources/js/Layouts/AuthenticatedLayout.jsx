d70ca1b0989bc54ba5009c9b1c1b5c7f2b1704a2
andidprastyo <2141720046@student.polinema.ac.id>
2025-06-11 23:38:56 +0700

    chore: Minor fix

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -16,8 +16,9 @@ export default function AuthenticatedLayout({ header, children }) {
                     <div className="flex h-16 justify-between">
                         <div className="flex">
                             <div className="flex shrink-0 items-center">
-                                <Link href="/">
-                                    <ApplicationLogo className="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
+                                <Link href="/dashboard" className="flex items-center space-x-3">
+                                    <img src="/Logo EEPROM nobg.png" alt="EEPROM Logo" className="h-12 w-auto" />
+                                    <span className="text-xl font-semibold text-gray-800 dark:text-gray-200">CMS</span>
                                 </Link>
                             </div>
                         </div>
